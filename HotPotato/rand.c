31ff45d8cdf237ac6acfe1ff2048ed9c2f9ff4e5
Matthew Gerstel <matt.gerstel@gmail.com>
2020-07-11 19:36:36 -0700

    made randomizer

--- HotPotato/rand.c
+++ HotPotato/rand.c
@@ -2,10 +2,11 @@
  * rand.c
  *
  * Created: 7/11/2020 6:21:09 PM
- *  Author: Spencer Comora
+ *  Author: Spencer Comora, Matthew Gerstel
  */ 
 
 #include "rand.h"
+#include "timer.h"
 
 void	Rand_Init(void)
 {
@@ -14,15 +15,25 @@ void	Rand_Init(void)
 
 uint8_t	Rand_GetRandomU8(void)
 {
-	return 0;
+	return (uint8_t)TCC0.CNT;
 }
 
 uint8_t Rand_GetRandomPowerOfTwoU8(void)
 {
-	return 0;
+	return Rand_RoundUpToNearestPowerOfTwoU8(Rand_GetRandomU8());
 }
 
 uint8_t Rand_RoundUpToNearestPowerOfTwoU8(uint8_t valuetoRound)
 {
-	return 0;
+	valuetoRound--;
+	valuetoRound |= valuetoRound >> 1;
+	valuetoRound |= valuetoRound >> 2;
+	valuetoRound |= valuetoRound >> 4;
+	valuetoRound |= valuetoRound >> 8;
+	valuetoRound |= valuetoRound >> 16;
+	valuetoRound |= valuetoRound >> 32;
+	valuetoRound |= valuetoRound >> 64;
+	valuetoRound |= valuetoRound >> 128;
+	valuetoRound++;
+	return valuetoRound;
 }
